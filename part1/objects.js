ece94e737ddadc8b6e9b7d42f90da046e1531e14
Majon1 <mathilda.nynas@gmail.com>
2022-01-22 12:28:35 +0200

    done

--- part1/objects.js
+++ part1/objects.js
@@ -1,7 +1,20 @@
-const object1 = {
+const arto = {
     name: 'Arto Hellas',
     age: 35,
     education: 'PhD',
+    greet: function () {
+        console.log('Hello, my name is ' + this.name)
+    },
+    doAddition: function (a, b) {
+        console.log(a + b)
+    },
+}
+setTimeout(arto.greet.bind(arto), 1000)
+
+arto.greet()
+
+arto.growOlder = function () {
+    this.age += 1
 }
 
 const object2 = {
@@ -19,9 +32,33 @@ const object3 = {
     department: 'Stanford University',
 }
 
-console.log(object1.name)
+console.log(arto.name)
 const fieldName = 'age'
-console.log(object1[fieldName])
+console.log(arto[fieldName])
 
-object1.address = 'Helsinki'
-object1['secret number'] = 12341
+arto.address = 'Helsinki'
+arto['secret number'] = 12341
+
+arto.growOlder()
+console.log(arto.age)
+
+arto.doAddition(1, 4)
+
+const referenceToAddition = arto.doAddition
+referenceToAddition(10, 15)
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    greet() {
+        console.log('hello, my name is ' + this.name)
+    }
+}
+
+const adam = new Person('Adam Ondra', 35)
+adam.greet()
+
+const janja = new Person('Janja Garnbret', 22)
+janja.greet()
